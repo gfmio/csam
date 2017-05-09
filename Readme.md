83c860a5bd2b056678b83171468403f44e1eb2cd
Frédérique Mittelstaedt <me@gfm.io>
2017-05-09 11:58:20 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+csam
+====
+
+**csam** is a fast and minimal component-based framework based on the SAM (state-action-model) pattern.
+
+Memory footprint so far: 16.5 MB for one instance
+Response time < 40ms
+
+# License: MIT
---
+++ index.js
@@ -0,0 +1,11 @@
+//
+// csam library entry point
+//
+
+module.exports = {
+  Model: require('./lib/model'),
+  State: require('./lib/state'),
+  Server: require('./lib/server'),
+  Client: require('./lib/client'),
+  Component: require('./lib/component'),
+}
